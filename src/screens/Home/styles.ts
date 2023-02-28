3bb30615980ff22792ab808c36efd8c95fa3eb28
Neto <Geraldo.Neto@aptiv.com>
2023-02-28 16:32:21 -0300

    Modificacoes

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -19,6 +19,41 @@ export const styles = StyleSheet.create({
         width:110,
         marginTop:70,
         marginLeft: 132
+    },
+    buttonText:{
+        height:54,
+        width:271,
+        borderRadius: 6,
+        paddingLeft: 16,
+        color: "#FFFFFF",
+        backgroundColor: "#262626",
+        textAlign: "left",
+        marginRight: 4
+    },
+    buttonAdd:{
+        height:52,
+        width:52,
+        borderRadius: 6,
+        backgroundColor: "#1E6F9F",
+        justifyContent:"center",
+        alignItems: "center"
+    },
+    buttonPlus:{
+        color: "#FFF",
+        fontSize: 24,
+    },
+    textAndButton:{
+        height:52,
+        width:52,
+        marginLeft:24,
+        marginRight:24,
+        color: "#FFFFFF",
+        flexDirection: "row",
+        marginTop: 40
+    },
+    plusImage:{
+        height: 16,
+        width: 16
     }
 
 })
